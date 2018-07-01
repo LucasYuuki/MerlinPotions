8e8b1e2c19b35820c1deb2fc7e09e83d06e7163e
LucasYuuki <39968236+LucasYuuki@users.noreply.github.com>
2018-07-01 15:50:37 -0300

    Versão 1
    versão 1

---
+++ java.js
@@ -0,0 +1,12 @@
+
+function Abrir()
+		{
+			document.getElementById('light').style.display= 'block';
+			document.getElementById('fade').style.display='block';
+		}
+
+		function Fechar()
+		{
+			document.getElementById('light').style.display='none';
+			document.getElementById('fade').style.display='none';
+		}
---
+++ js/java.js
@@ -0,0 +1,12 @@
+
+function Abrir()
+		{
+			document.getElementById('box').style.display= 'block';
+			document.getElementById('fd').style.display='block';
+		}
+
+		function Fechar()
+		{
+			document.getElementById('box').style.display='none';
+			document.getElementById('fd').style.display='none';
+		}
